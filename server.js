d4f6f6a70577fc3513590c699515e514db127432
Kesava Mallela <kesava@gmail.com>
2014-04-07 11:17:17 -0700

    for heroku

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 var express = require('express');
+var logfmt = require("logfmt");
 var app = express();
-port = process.argv[2] || 8000;
 
-app.configure(function () {
-    app.use(
-        "/", //the URL throught which you want to access to you static content
-        express.static(__dirname) //where your static content is located in your filesystem
-    );
+app.use(logfmt.requestLogger());
+var port = Number(process.env.PORT || 5000);
+
+app.listen(port, function() {
+  console.log("Listening on " + port);
 });
-app.listen(port); //the port you want to use
-console.log("Express server running at => http://localhost:" + port + "/\nCTRL + C to shutdown");
